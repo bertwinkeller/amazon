ac1c04714f3c6cdce1403c6066e215c54dfe0269
bertwinkeller <bertwinkeller@gmail.com>
2022-12-09 12:37:22 -0800

    updated routes

--- index.js
+++ index.js
@@ -27,4 +27,34 @@ app.get("/products/:productId", async (req, res) => {
   }
 })
 
+app.get("/products/:productId/reviews", async (req, res) => {
+    const { productId } = req.params
+  
+    try {
+      const response = await request(
+        `${baseUrl}&url=https://www.amazon.com/product-reviews/${productId}`
+      )
+      res.json(JSON.parse(response))
+    } catch (error) {
+      res.json(error)
+    }
+  })
+
+  app.get("/products/:productId/offers", async (req, res) => {
+    const { productId } = req.params
+  
+    try {
+      const response = await request(
+        `${baseUrl}&url=https://www.amazon.com/gp/offer-listing/${productId}`
+      )
+      res.json(JSON.parse(response))
+    } catch (error) {
+      res.json(error)
+    }
+  })
+
+
+
+
+
 app.listen(PORT, () => console.log(`Server Running on Port: ${PORT}`))
