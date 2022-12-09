2c58539404ba9612a192a58ba0f1cb4d99c5202c
bertwinkeller <bertwinkeller@gmail.com>
2022-12-08 22:44:10 -0800

    env file

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
-const express = require('express')
-const request = require('request-promise')
+const express = require("express")
+const request = require("request-promise")
+require('dotenv').config()
 
 const app = express()
 const PORT = process.env.PORT || 5000
 
+ const apiKey = process.env.API_KEY
+const baseUrl = `http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`
 
 app.use(express.json())
-app.get('/', (req, res) => {
-    res.send(' Welcome to Amazon Scraper API')
+app.get("/", (req, res) => {
+  res.send("Welcome to Amazon Scraper API")
 })
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+//Get Product Details
+app.get("/products/:productId", async (req, res) => {
+  const { productId } = req.params
 
+  try {
+    const response = await request(
+      `${baseUrl}&url=https://www.amazon.com/dp/${productId}`
+    )
+    res.json(JSON.parse(response))
+  } catch (error) {
+    res.json(error)
+  }
+})
 
-
+app.listen(PORT, () => console.log(`Server Running on Port: ${PORT}`))
